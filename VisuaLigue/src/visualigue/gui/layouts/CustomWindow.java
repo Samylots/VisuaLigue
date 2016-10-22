a9658f2ec45d42d88b9befbaff85240ea0ec8048
Samylots <samapoil9@hotmail.com>
2016-10-22 12:50:12 -0400

    Documents pour avancement + fichier oubliés

---
+++ VisuaLigue/src/visualigue/gui/layouts/CustomWindow.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visualigue.gui.layouts;
+
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author samap
+ */
+public class CustomWindow extends Stage {
+
+    public CustomWindow(Node parent, Parent node) {
+        super();
+        this.initModality(Modality.WINDOW_MODAL);
+        this.initOwner(parent.getScene().getWindow());
+        Scene scene = new Scene(node);
+        this.setScene(scene);   
+    }
+}
