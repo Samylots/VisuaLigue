be0ddda886341774368050bce42d4ce9304e937b
Bruno Le Dû Lecavalier <bruno.le-du-lecavalier.1@ulaval.ca>
2016-11-26 00:20:29 -0500

    labels sous players

--- VisuaLigue/src/visualigue/gui/javafx/fxdrawers/GameDrawer.java
+++ VisuaLigue/src/visualigue/gui/javafx/fxdrawers/GameDrawer.java
@@ -6,12 +6,18 @@
 package visualigue.gui.javafx.fxdrawers;
 
 import java.util.List;
+import javafx.geometry.VPos;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import visualigue.VisuaLigue;
 import visualigue.utils.Coords;
 import visualigue.gui.javafx.fxcontrollers.VisuaLigueBoard;
 import visualigue.dto.*;
 import visualigue.utils.Dimension;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 /**
  *
@@ -31,9 +37,6 @@ public class GameDrawer {
         positions.stream().forEach((position) -> {
             if (position.entity instanceof PlayerDTO) {
                 drawEntity(getPixelPosition(position.coords), position.entity);
-                if (VisuaLigue.domain.isShowingRoles()) {
-                    //TODO draw roles/names etc.
-                }
             } else if (position.entity instanceof ObstacleDTO) {
                 drawEntity(getPixelPosition(position.coords), position.entity);
             } else if (position.entity instanceof AccessoryDTO) {
@@ -46,7 +49,28 @@ public class GameDrawer {
 
     private void drawEntity(Coords coords, EntityDTO entity) {
         Dimension dim = canvas.getConverter().meterToPixel(entity.dimension, canvas.getActualFieldPixelDimension());
-        canvas.getGraphicsContext2D().drawImage(new Image(entity.picturePath), coords.getX(), coords.getY(), dim.getWidth(), dim.getHeight());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        double coordsX = coords.getX();
+        double coordsY = coords.getY();
+        double width = dim.getWidth();
+        double height = dim.getHeight();
+        
+        gc.drawImage(new Image(entity.picturePath), coordsX, coordsY, width, height);
+        
+        if (entity instanceof PlayerDTO && VisuaLigue.domain.isShowingRoles()) {
+            PlayerDTO player = (PlayerDTO)entity;
+            
+            gc.setFont(Font.font(14));
+            gc.setStroke(Color.BLACK);
+            gc.setFill(Color.WHITE);
+
+            gc.strokeText(player.role, coordsX, coordsY+height);
+            gc.fillText(player.role, coordsX, coordsY+height);
+            
+            gc.setFont(Font.font(12));
+            gc.strokeText(player.name, coordsX, coordsY+height+16);
+            gc.fillText(player.name, coordsX, coordsY+height+16);
+        }
     }
 
     private Coords getPixelPosition(Coords domainCoords) {
