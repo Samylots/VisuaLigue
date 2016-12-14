960990bd36f255299d3625fed7dfc66e5479f9fc
sbeaudoin <sbeaudoin@ubeo.ca>
2016-12-14 13:05:59 -0500

    oubli

---
+++ VisuaLigue/src/visualigue/gui/javafx/fxcontrollers/states/LoginPaneController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visualigue.gui.javafx.fxcontrollers.states;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Samuel
+ */
+public class LoginPaneController implements Initializable {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Button loginButton;
+
+    private boolean isValidLogin = false;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void login(ActionEvent event) {
+    }
+
+    public boolean isValidLogin() {
+        return isValidLogin;
+    }
+
+}
